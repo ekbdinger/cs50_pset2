fb3d62485d682c5a269495c57d63b9d8eec467a4
Paul <paulbosox@yahoo.com>
2012-12-06 15:46:05 -0500

    removed spaces, extra comments and old code that was commented out

--- dec_caesar6.c
+++ dec_caesar6.c
@@ -11,6 +11,7 @@
 
     // lowercase alphabet
     string lower[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    // string upper[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
  
  // Get user input, look at string1.c for an example from lecture 2w
     printf("Please enter a phrase:  ");
@@ -26,7 +27,7 @@
             printf("%c:  ", p);   // type back the text via an array
             // printf("%d ", (int) p);
             
-            // int (p) - 97, this tells us what place in the alphabet the letter is
+            //  this tells us what place in the alphabet the letter is, int (p) - 97, array is indexed at one
             int fred = (int) p - 97;
             printf("%c is the %d number in the alphabet", p, fred);
     
@@ -39,26 +40,7 @@
             
             // print out the letter in the array
             printf("The value of %c is now %s\n", p, lower[c]);
-            
-            
-            
-            
-          // i think this is what I want to replace; int j = (k % 26);  // adding this
-  /*          
-            printf("+ k = %d", ((int) p) + j);   // might want to turn this into a variable  (c + k)
-            printf(":  %c\n", (p + j));
-   */         
-         /*   // Enciphering equation
-            printf("Enciphering equation\n\n");
-            
-            int c;
-            c = ((int) p + k) % 26;
-            printf("The value is:  %d", c);
-            
-         // this is the part I need to focus on
-         
-         */
-   
+ 
         }
         printf("\n\n\n");
 
